c1963ede53e635c4f040827edb0bb4659c9e9b9b
albiebabie <albiebabie@gmail.com>
2016-09-09 12:59:41 +0100

    alban cannot run emulator

---
+++ app/src/main/java/com/team/platformerrun/CollectCoins.java
@@ -0,0 +1,22 @@
+package com.team.platformerrun;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CollectCoins extends AppCompatActivity {
+
+    MediaPlayer coinSound;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collect_coins);
+        coinSound = MediaPlayer.create(this, R.raw.coin_sound );
+    }
+
+    public void playSound() {
+        coinSound.start();
+    }
+
+}
