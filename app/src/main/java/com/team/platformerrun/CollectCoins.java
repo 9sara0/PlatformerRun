614497c86f5a5b009126a16036fa39b9744f502e
Sara <sarah.alalaya@gmail.com>
2016-09-09 15:14:47 +0300

    By pressing a "collect coin" button, a "coind sound" audio is played

--- app/src/main/java/com/team/platformerrun/CollectCoins.java
+++ app/src/main/java/com/team/platformerrun/CollectCoins.java
@@ -3,6 +3,7 @@ package com.team.platformerrun;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CollectCoins extends AppCompatActivity {
 
@@ -15,7 +16,7 @@ public class CollectCoins extends AppCompatActivity {
         coinSound = MediaPlayer.create(this, R.raw.coin_sound );
     }
 
-    public void playSound() {
+    public void playSound(View view) {
         coinSound.start();
     }
 
