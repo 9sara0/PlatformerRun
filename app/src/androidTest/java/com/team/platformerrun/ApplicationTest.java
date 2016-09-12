8b80abeedb6203d90e9cd9dc1705980b0e85af16
Sara <sarah.alalaya@gmail.com>
2016-09-12 20:04:48 +0300

    unresolved feature testing error

--- app/src/androidTest/java/com/team/platformerrun/ApplicationTest.java
+++ app/src/androidTest/java/com/team/platformerrun/ApplicationTest.java
@@ -1,20 +1,48 @@
 package com.team.platformerrun;
 
 import android.app.Application;
+
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
+
+import android.test.suitebuilder.annotation.LargeTest;
+
+import dalvik.annotation.TestTarget;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static java.util.regex.Pattern.matches;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+
+
+
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
 
+    @Rule
+    public ActivityTestRule<HomeActivity> myActivittyRule = new ActivityTestRule<>(HomeActivity.class);
+
+    @Test
+    public void startNewGame_secondActivity(){
+        onView(withId(R.id.startGame))
+                .perform(click());
 
 
-    onView(withId(R.id.my_view))            // withId(R.id.my_view) is a ViewMatcher
-            .perform(click())               // click() is a ViewAction
-            .check(matches(isDisplayed())); // matches(isDisplayed()) is a ViewAssertion
+
+    }
 
 }
